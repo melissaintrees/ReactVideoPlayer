152282e55942ad6596495249e9ee2b2316374f7e
melissaintrees <melissaintrees@gmail.com>
2018-12-25 09:40:21 -0600

    Updates

---
+++ precache-manifest.42af547ee2ad91e00f8bb1fb7a0270e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4001bd85412afd7c9cb",
+    "url": "/ReactVideoPlayer/static/js/runtime~main.e4001bd8.js"
+  },
+  {
+    "revision": "ccb16172e91e7de35654",
+    "url": "/ReactVideoPlayer/static/js/main.ccb16172.chunk.js"
+  },
+  {
+    "revision": "102c3bf24ee7bc42981e",
+    "url": "/ReactVideoPlayer/static/js/1.102c3bf2.chunk.js"
+  },
+  {
+    "revision": "ccb16172e91e7de35654",
+    "url": "/ReactVideoPlayer/static/css/main.8f799280.chunk.css"
+  },
+  {
+    "revision": "aeb54cceba5986e1b628a8b0cd9ece6e",
+    "url": "/ReactVideoPlayer/index.html"
+  }
+];
